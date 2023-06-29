c4bbf6d432b916ba246f2fd2215303450d6e34ac
lixiaoyao <15111009275@163.com>
2023-06-29 14:11:16 +0800

    fix: commitlint和markdownlint删除test文件

--- packages/xiaoyao-commitlint-config/__tests__/xiaoyao-commitlint-config.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const xiaoyaoCommitlintConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(xiaoyaoCommitlintConfig(), 'Hello from xiaoyaoCommitlintConfig');
-console.info('xiaoyaoCommitlintConfig tests passed');
--- packages/xiaoyao-markdownlint-config/__tests__/xiaoyao-markdownlint-config.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const xiaoyaoMarkdownlintConfig = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(xiaoyaoMarkdownlintConfig(), 'Hello from xiaoyaoMarkdownlintConfig');
-console.info('xiaoyaoMarkdownlintConfig tests passed');
